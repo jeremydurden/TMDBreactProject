c23f941f4f48c5028905b8c633331d50e1210452
Jeremy Durden <jeremydurden@gmail.com>
2021-07-10 16:33:15 -0700

    creating HeroImage and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import API from "../API";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
-
+import HeroImage from "./HeroImage";
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 // Image
@@ -13,8 +13,18 @@ import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
-  console.log("this is state", state);
-  return <div>Home Page</div>;
+
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
