4be90a3faceeb4009cfa0495a30b54c9f2aa4237
Jeremy Durden <jeremydurden@gmail.com>
2021-07-10 15:23:46 -0700

    making first API call with useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+// API
+import API from "../API";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
@@ -13,6 +15,21 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+    } catch (error) {
+      setError(true);
+    }
+  };
+  // initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
   return <div>Home Page</div>;
 };
 
